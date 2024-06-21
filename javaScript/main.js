6de4a5bec3265ba7a78b6b30fc85182607ec17da
Rebecamo <daynarebeca12@gmail.com>
2024-06-20 20:13:11 -0600

    cambios

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,77 +1,77 @@
 import Pokedex from './pokedex.js';
 
-document.addEventListener('DOMContentLoaded', async() =>{
+document.addEventListener('DOMContentLoaded', async () => {
     const miPokedex = new Pokedex();
     await miPokedex.obtenerTodosLosPokemon();
     miPokedex.dibujarPokedex();
 
-    //manjera la navegacion entre secciones
+    // Manejar la navegación entre secciones
     const sectionLinks = document.querySelectorAll('nav a[data-section]');
-    sectionLinks.forEach(link =>{
-        link.addEventListener('click', (event) =>{
+    sectionLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
             event.preventDefault();
             const sectionId = event.target.getAttribute('data-section');
             showSection(sectionId);
         });
     });
 
-    //manejar el clic en el enlace "Ver todos"
+    // Manejar el clic en el enlace "Ver todos"
     const verTodosLink = document.getElementById('ver-todos');
-    verTodosLink.addEventListener('click', (event) =>{
+    verTodosLink.addEventListener('click', (event) => {
         event.preventDefault();
         showSection('pokedex');
         mostrarTodosLosPokemon();
     });
 
-    //manejar el clic en las etiquetas de tipo de Pokemon
+    // Manejar el clic en las etiquetas de tipo de Pokémon
     const tipoLinks = document.querySelectorAll('.nav_vertical li a[data-tipo]');
-    tipoLinks.forEach(link =>{
-        link.addEventListener('click', (event)=>{
+    tipoLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
             event.preventDefault();
             const tipo = event.target.getAttribute('data-tipo');
             showSection('pokedex');
             filtrarPorTipo(tipo);
         });
     });
-    //funcion para mostra una seccion y ocultar las demas
-    function showSection(sectionId){
+
+    // Función para mostrar una sección y ocultar las otras
+    function showSection(sectionId) {
         const sections = document.querySelectorAll('main section');
-        sections.forEach(section =>{
+        sections.forEach(section => {
             section.classList.remove('active');
         });
         const targetSection = document.getElementById(sectionId);
-        if(targetSection){
+        if (targetSection) {
             targetSection.classList.add('active');
         }
     }
 
-    //funcion para mostrar los pokemon
-
-    function mostrarTodosLosPokemon(){
+    // Función para mostrar todos los Pokémon
+    function mostrarTodosLosPokemon() {
         const pokedexSection = document.getElementById('pokedex');
-        pokedexSection.innerHTML = ''; //limpia la pokedex antes de mostrar todos los pokemons
+        pokedexSection.innerHTML = ''; // Limpia la Pokédex antes de mostrar todos los Pokémon
 
-        miPokedex.pokemons.forEach(pokemon =>{
+        miPokedex.pokemons.forEach(pokemon => {
             const pokemonDiv = pokemon.dibujarPokemon();
             pokedexSection.appendChild(pokemonDiv);
         });
     }
 
-    //funcion para filtrar y mostrar pokemon por tipo
-
-    function filtrarPorTipo(tipo){
-        const pokemonFiltrados = miPokedex.pokemons.filter(pokemon =>{
+    // Función para filtrar y mostrar Pokémon por tipo
+    function filtrarPorTipo(tipo) {
+        const pokemonFiltrados = miPokedex.pokemons.filter(pokemon => {
             return pokemon.tipo.toLowerCase().includes(tipo);
         });
 
-        const pokedexSection = document.getElementById('pokdex');
+        const pokedexSection = document.getElementById('pokedex');
         pokedexSection.innerHTML = '';
 
-        pokemonFiltrados.forEach(pokemon=>{
+        pokemonFiltrados.forEach(pokemon => {
             const pokemonDiv = pokemon.dibujarPokemon();
             pokedexSection.appendChild(pokemonDiv);
         });
     }
-    //mostrar la seccion de inicio al cargar la pagina
+
+    // Mostrar la sección de inicio al cargar la página
     showSection('inicio');
 });
